11538b9476925c3b8d00a7eac7a18d9ea802db29
Maxi Redigonda <maxiredigonda@gmail.com>
2023-02-10 09:30:39 -0300

    Move encrypt/decrypt to their own functions

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use inquire::Select;
 use inquire::Text;
-use std::process::Command;
 use std::path::Path;
+use std::process::Command;
 
 fn main() {
     let options = vec!["Encrypt", "Decrypt"];
@@ -27,34 +27,9 @@ fn main() {
     }
 
     if ans == "Encrypt" {
-        // Run the following command: gpg --batch --passphrase "<passphrase>" -c <filename>
-        let mut child = Command::new("gpg")
-            .arg("--batch")
-            .arg("--passphrase")
-            .arg(passphrase)
-            .arg("-c")
-            .arg(filename)
-            .spawn()
-            .expect("Encrypting the file...");
-        let exit_status = child.wait().expect("Encrypting the file...");
-        if !exit_status.success() {
-            println!("Something went wrong");
-            return;
-        }
+        encrypt(filename, passphrase);
     } else {
-        // Run the following command: gpg --passphrase "<passphrase>" <filename>
-        let mut child = Command::new("gpg")
-            .arg("--batch")
-            .arg("--passphrase")
-            .arg(passphrase)
-            .arg(filename)
-            .spawn()
-            .expect("Decrypting the file...");
-        let exit_status = child.wait().expect("Decrypting the file...");
-        if !exit_status.success() {
-            println!("Wrong passphrase");
-            return;
-        }
+        decrypt(filename, passphrase);
     }
 
     // Save to file
@@ -62,4 +37,37 @@ fn main() {
     // Done
 
     println!("You're welcome");
+}
+
+fn encrypt(filename: String, passphrase: String) {
+    // Run the following command: gpg --batch --passphrase "<passphrase>" -c <filename>
+    let mut child = Command::new("gpg")
+        .arg("--batch")
+        .arg("--passphrase")
+        .arg(passphrase)
+        .arg("-c")
+        .arg(filename)
+        .spawn()
+        .expect("Encrypting the file...");
+    let exit_status = child.wait().expect("Encrypting the file...");
+    if !exit_status.success() {
+        println!("Failed to encrypt the file.");
+        return;
+    }
+}
+
+fn decrypt(filename: String, passphrase: String) {
+    // Run the following command: gpg --passphrase "<passphrase>" <filename>
+    let mut child = Command::new("gpg")
+        .arg("--batch")
+        .arg("--passphrase")
+        .arg(passphrase)
+        .arg(filename)
+        .spawn()
+        .expect("Decrypting the file...");
+    let exit_status = child.wait().expect("Decrypting the file...");
+    if !exit_status.success() {
+        println!("Failed to decrypt the file.");
+        return;
+    }
 }
